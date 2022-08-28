768aa796e0d9b17cbeccc096c3cff854d11aece0
tetraf0ur <artem131999@gmail.com>
2022-08-28 20:04:42 +0300

    supply(complete without adaptive)

---
+++ src/components/Faq.tsx
@@ -0,0 +1,149 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const DATA = [
+  {
+    title: "What are event contracts?",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "How does trading work?",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "How is the market price determined?",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "Who am I trading with?",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "How is BorB regulated?",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "How are customer deposits processed?",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "Is this gambling?",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+];
+
+const Faq = () => {
+  return (
+    <FaqWrapper>
+      <FaqTitle>FAQ</FaqTitle>
+      <FaqList>
+        {DATA.map(({ title, subtitle }, id) => (
+          <FaqItemWrapper key={id} title={title} subtitle={subtitle} />
+        ))}
+      </FaqList>
+    </FaqWrapper>
+  );
+};
+
+const FaqItemWrapper = ({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle: string;
+}) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <FaqItem onClick={() => setOpen((prev) => !prev)}>
+      <Title>{title}</Title>
+      <PlusIcon open={open} />
+      <Article open={open}>{subtitle}</Article>
+    </FaqItem>
+  );
+};
+
+const FaqWrapper = styled.div``;
+
+const FaqTitle = styled.h3`
+  font-family: "Inter";
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 140%;
+  color: #23272b;
+  margin-bottom: 24px;
+`;
+const FaqList = styled.ul``;
+
+const FaqItem = styled.li`
+  border-bottom: 1px solid #e9ecf2;
+  margin-bottom: 24px;
+  position: relative;
+`;
+
+const Title = styled.h4`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 140%;
+  color: #23272b;
+`;
+const Article = styled.p<{ open: boolean }>`
+  font-family: "Inter";
+  font-style: normal;
+  overflow: hidden;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 160%;
+  padding-top: 24px;
+  color: #23272b;
+  max-height: ${({ open }) => (open ? "500px" : "0")};
+  margin-bottom: ${({ open }) => (open ? "24px" : "0")};
+
+  transition: all 0.2s ease;
+`;
+
+const PlusIcon = styled.span<{ open: boolean }>`
+  width: 20px;
+  height: 20px;
+
+  position: absolute;
+  right: 0;
+  top: 1px;
+  display: flex;
+
+  &::before,
+  &::after {
+    position: absolute;
+    height: 1.5px;
+    width: 100%;
+    border-radius: 2px;
+  }
+
+  &::before {
+    content: "";
+    top: 50%;
+    left: 0px;
+    transform: translateY(-50%);
+    background-color: #23272b;
+  }
+
+  &::after {
+    content: "";
+    top: -0.5px;
+
+    background-color: ${({ open }) => (!open ? "#23272b" : "transparent")};
+
+    transform: rotate(-90deg) translateX(-50%);
+  }
+`;
+
+export { Faq };
--- src/pages/SupplyPage/index.tsx
+++ src/pages/SupplyPage/index.tsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { ReactComponent as InfoIcon } from "../../assets/info-icon.svg";
+import { ReactComponent as USDCIcon } from "../../assets/usdc-icon.svg";
+import { ReactComponent as ArrowIcon } from "../../assets/arrow-down-icon.svg";
+import { Faq } from "../../components/Faq";
 
 const SupplyPage = () => {
   return (
@@ -16,19 +20,68 @@ const SupplyPage = () => {
         </Tab>
       </TabContainer>
       <InputContainer>
-        <div className="wrapper">
+        <InputWrapper>
           <SettingsTitle margin="9px">Supply</SettingsTitle>
           <div className="input-wrapper">
+            <SelectWrapper>
+              <CurrencyWrapper>
+                <USDCIcon />
+              </CurrencyWrapper>
+              <span>USTC</span>
+              <ArrowIcon />
+            </SelectWrapper>
             <input type="number" className="input" placeholder="Amount" />
           </div>
-        </div>
-        <div className="wrapper">
+        </InputWrapper>
+        <InputWrapper disabled>
           <SettingsTitle margin="9px">Receive</SettingsTitle>
           <div className="input-wrapper">
-            <input type="number" className="input" />
+            <SelectWrapper>
+              <CurrencyWrapper>
+                <USDCIcon />
+              </CurrencyWrapper>
+              <span>USTC+</span>
+            </SelectWrapper>
+            <input type="number" className="input" disabled value={0} />
           </div>
-        </div>
+        </InputWrapper>
       </InputContainer>
+      <SettingsTitle margin="24px">Balance: 263</SettingsTitle>
+      <Btn>Supply USDC</Btn>
+      <PurchaseWrapper>
+        <PurchaseDataList>
+          <PurchaseDataItem>
+            <SettingsTitle>Exchange rate:</SettingsTitle>
+          </PurchaseDataItem>
+          <PurchaseDataItem>
+            <SettingsTitle>Deposit fee:</SettingsTitle>
+          </PurchaseDataItem>
+          <PurchaseDataItem>
+            <SettingsTitle>Projected APY:</SettingsTitle>
+          </PurchaseDataItem>
+        </PurchaseDataList>
+        <PurchaseDataList>
+          <PurchaseDataItem>
+            <SettingsTitle>
+              1USDC=1USDC+
+              <InfoIcon />
+            </SettingsTitle>
+          </PurchaseDataItem>
+          <PurchaseDataItem>
+            <SettingsTitle>
+              0%
+              <InfoIcon />
+            </SettingsTitle>
+          </PurchaseDataItem>
+          <PurchaseDataItem>
+            <SettingsTitle>
+              5.79%
+              <InfoIcon />
+            </SettingsTitle>
+          </PurchaseDataItem>
+        </PurchaseDataList>
+      </PurchaseWrapper>
+      <Faq />
     </div>
   );
 };
@@ -87,18 +140,26 @@ const Tab = styled.div<{ active?: boolean }>`
   }
 `;
 
-const SettingsTitle = styled.span<{ margin: string }>`
+const SettingsTitle = styled.span<{ margin?: string }>`
   font-family: "Inter";
   font-weight: 400;
   font-size: 14px;
   line-height: 140%;
-  margin-bottom: ${(props) => props.margin};
+  margin-bottom: ${(props) => props.margin ?? "0px"};
   color: #8a8f99;
+  display: flex;
+  /* align-items: center; */
+
+  svg {
+    margin-left: 8px;
+    margin-top: 3px;
+  }
 `;
 
 const InputContainer = styled.div`
   gap: 16px;
   display: flex;
+  margin-bottom: 12px;
 
   .title {
     font-family: "Inter";
@@ -106,7 +167,9 @@ const InputContainer = styled.div`
     font-size: 14px;
     line-height: 140%;
   }
+`;
 
+const InputWrapper = styled.div<{ disabled?: boolean }>`
   .wrapper {
     display: flex;
     flex-direction: column;
@@ -115,16 +178,20 @@ const InputContainer = styled.div`
   .input-wrapper {
     width: 340px;
     border: 1px solid #e9ecf2;
+    border: ${({ disabled }) =>
+      disabled ? "1px solid transparent" : "1px solid #e9ecf2"};
     border-radius: 8px;
     padding: 12px;
     display: flex;
+
+    background-color: ${({ disabled }) =>
+      disabled ? "#F6F8FC" : "transparent"};
   }
 
   .input {
     font-family: "Inter";
     font-weight: 400;
     font-size: 21px;
-    /* line-height: 30px; */
     color: #c2c5cc;
     text-align: right;
 
@@ -143,8 +210,47 @@ const InputContainer = styled.div`
 `;
 
 const SelectWrapper = styled.div`
-  
+  display: flex;
+  align-items: center;
 
+  span {
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 140%;
+    color: #23272b;
+  }
 `;
+
+const CurrencyWrapper = styled.div`
+  margin-right: 4px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Btn = styled.button`
+  background: #3fe7be;
+  border-radius: 8px;
+  padding: 13px 48px;
+  font-family: "Inter";
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 140%;
+  color: #238069;
+  align-self: flex-start;
+  margin-bottom: 37px;
+`;
+const PurchaseWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 80px;
+`;
+
+const PurchaseDataList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+const PurchaseDataItem = styled.li``;
 
 export { SupplyPage };
