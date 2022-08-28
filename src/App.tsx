957ea6f8e4cbb5af6e3a28670aaed5ffc213dbb7
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2022-08-28 16:05:11 +0300

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { SupplyPage } from "./pages/SupplyPage";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import AboutPage from './pages/AboutPage'
+import EarnPage from './pages/EarnPage'
+import { SupplyPage } from './pages/SupplyPage'
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<div>home page</div>} />
-      <Route path="/supply" element={<SupplyPage />} />
-    </Routes>
-  );
-};
+    return (
+        <div className="App">
+            <Nav />
+            <Routes>
+                <Route path="/" element={<div>home page</div>} />
+                <Route path="/supply" element={<SupplyPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/earn" element={<EarnPage />} />
+            </Routes>
+        </div>
+    )
+}
 
-export default App;
+export default App
