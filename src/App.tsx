c0557ea676ceb2ebd34da1790b6a68d7b96c4b7e
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2022-08-29 00:14:28 +0300

    fix: routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Nav from './components/Nav'
 import AboutPage from './pages/AboutPage'
 import EarnPage from './pages/EarnPage'
+import Home from './pages/HomePage'
 import { SupplyPage } from './pages/SupplyPage'
 
 const App = () => {
@@ -19,7 +20,7 @@ const App = () => {
                 <Header setIsBurger={setIsBurger} />
                 <div className="container">
                     <Routes>
-                        <Route path="/" element={<div>home page</div>} />
+                        <Route path="/" element={<Home />} />
                         <Route path="/supply" element={<SupplyPage />} />
                         <Route path="/about" element={<AboutPage />} />
                         <Route path="/earn" element={<EarnPage />} />
