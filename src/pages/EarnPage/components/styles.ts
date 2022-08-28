f886fcaaf4a6b2d0a7ec7c1b2b83d45565b2267d
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2022-08-28 18:33:01 +0300

    earn page

---
+++ src/pages/EarnPage/components/styles.ts
@@ -0,0 +1,124 @@
+import styled from 'styled-components'
+
+const StyledEarn = styled.div``
+
+const Title = styled.h4`
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 50px;
+`
+
+const SubText = styled.p`
+    margin-bottom: 48px;
+    font-size: 14px;
+    font-weight: 400;
+    color: var(--grey60);
+`
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 162px;
+    grid-gap: 12px;
+`
+
+const Card = styled.div`
+    padding: 24px;
+    border-radius: 16px;
+    display: flex;
+    flex-direction: column;
+    background: var(--grey99);
+`
+
+const Row = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const RowText = styled.div`
+    margin-left: 6px;
+    color: var(--grey60);
+    font-size: 14px;
+    margin-bottom: 8px;
+    font-weight: 400;
+`
+
+const CardLink = styled.p``
+
+const CardSubLink = styled.div`
+    margin-top: auto;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    p {
+        color: var(--green);
+        font-size: 14px;
+        font-weight: 600;
+        margin-right: 4px;
+    }
+`
+
+const CardContentText = styled.p`
+    margin-top: 8px;
+    font-size: 24px;
+    font-weight: 600;
+`
+const SmallCardContent = styled.p`
+    font-weight: 600;
+    font-size: 16px;
+    margin-top: 16px;
+`
+
+const RewardTitle = styled.h6`
+    margin-top: 62px;
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 22px;
+`
+
+const TableGrid = styled.div``
+
+const TableHead = styled.div``
+
+const TableRow = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-row-gap: 24px;
+`
+
+const Item = styled.div`
+    display: flex;
+    align-items: center;
+    p { 
+        font-weight: 400;
+        font-size: 14px;
+        
+        &:nth-child(2){
+            margin-left: 20%;
+        }
+        &:nth-child(3){
+            margin-left: auto;
+        }
+    }
+`
+
+export {
+    Item,
+    TableRow,
+    TableGrid,
+    TableHead,
+    RewardTitle,
+    StyledEarn,
+    Title,
+    CardContentText,
+    SmallCardContent,
+    SubText,
+    Grid,
+    Card,
+    Row,
+    RowText,
+    CardLink,
+    CardSubLink,
+}
--- src/pages/EarnPage/index.jsx
+++ src/pages/EarnPage/index.jsx
@@ -1,9 +1,102 @@
 import React from 'react'
+import {
+    Card,
+    CardContentText,
+    CardSubLink,
+    Grid,
+    Item,
+    RewardTitle,
+    Row,
+    RowText,
+    SmallCardContent,
+    StyledEarn,
+    SubText,
+    TableGrid,
+    TableHead,
+    TableRow,
+    Title,
+} from './components/styles'
 
 const EarnPage = () => {
-  return (
-    <div>index</div>
-  )
+    return (
+        <StyledEarn>
+            <Title>Earn crypto by referring friends</Title>
+            <SubText>
+                Share your referal link, invite friend and you'll be rewarded
+                with 50% of their trading fees
+            </SubText>
+
+            <Grid>
+                <Card>
+                    <Row>
+                        <img src="/images/earn/link.svg" alt="" />
+                        <RowText>Referal</RowText>
+                    </Row>
+                    <SmallCardContent>
+                        borb.fi/?ref=BgS...t73yhz7olv
+                    </SmallCardContent>
+                    <CardSubLink>
+                        <p>Copy</p>
+                        <img src="/images/earn/copy.svg" alt="" />
+                    </CardSubLink>
+                </Card>
+                <Card>
+                    <Row>
+                        <img src="/images/earn/usdc_logo.svg" alt="" />
+                        <RowText>USDC</RowText>
+                    </Row>
+                    <CardContentText>$4673</CardContentText>
+                    <CardSubLink>
+                        <p>Copy</p>
+                        <img src="/images/earn/arrow.svg" alt="" />
+                    </CardSubLink>
+                </Card>
+                <Card>
+                    <Row>
+                        <img src="/images/earn/usdt_logo.svg" alt="" />
+                        <RowText>USDT</RowText>
+                    </Row>
+                    <CardContentText>$740</CardContentText>
+                    <CardSubLink>
+                        <p>Copy</p>
+                        <img src="/images/earn/arrow.svg" alt="" />
+                    </CardSubLink>
+                </Card>
+            </Grid>
+
+            <RewardTitle>Reward</RewardTitle>
+            <TableGrid>
+                <TableHead></TableHead>
+                <TableRow>
+                    <Item>
+                        <p>0x24e78dvb8ybsalUGHGbdc98e3er3</p>
+                        <p>USDC</p>
+                        <p>$23</p>
+                    </Item>
+                    <Item>
+                        <p>0x24e78dvb8ybsalUGHGbdc98e3er3</p>
+                        <p>USDC</p>
+                        <p>$23</p>
+                    </Item>
+                    <Item>
+                        <p>0x24e78dvb8ybsalUGHGbdc98e3er3</p>
+                        <p>USDC</p>
+                        <p>$23</p>
+                    </Item>
+                    <Item>
+                        <p>0x24e78dvb8ybsalUGHGbdc98e3er3</p>
+                        <p>USDC</p>
+                        <p>$23</p>
+                    </Item>
+                    <Item>
+                        <p>0x24e78dvb8ybsalUGHGbdc98e3er3</p>
+                        <p>USDC</p>
+                        <p>$23</p>
+                    </Item>
+                </TableRow>
+            </TableGrid>
+        </StyledEarn>
+    )
 }
 
 export default EarnPage
