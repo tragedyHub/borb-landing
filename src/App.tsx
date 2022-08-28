a0b665586c9aea4dd71143dae57140fc3f64d042
tetraf0ur <artem131999@gmail.com>
2022-08-28 12:38:42 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { SupplyPage } from "./pages/SupplyPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>home page</div>} />
+      <Route path="/supply" element={<SupplyPage />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/pages/SupplyPage/index.tsx
+++ src/pages/SupplyPage/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const SupplyPage = () => {
   return (
-    <div>
+    <div className="container">
       <Title>Supply crypto and earn interest</Title>
       <Subtitle>
         Supply your tokens and get Token+ while earning interest
@@ -15,6 +15,20 @@ const SupplyPage = () => {
           <span>Withdraw</span>
         </Tab>
       </TabContainer>
+      <InputContainer>
+        <div className="wrapper">
+          <SettingsTitle margin="9px">Supply</SettingsTitle>
+          <div className="input-wrapper">
+            <input type="number" className="input" placeholder="Amount" />
+          </div>
+        </div>
+        <div className="wrapper">
+          <SettingsTitle margin="9px">Receive</SettingsTitle>
+          <div className="input-wrapper">
+            <input type="number" className="input" />
+          </div>
+        </div>
+      </InputContainer>
     </div>
   );
 };
@@ -38,19 +52,92 @@ const Subtitle = styled.h2`
 `;
 
 const TabContainer = styled.div`
-  border-bottom: 1px solid #e9ecf2;
+  display: flex;
+
+  position: relative;
+  gap: 57px;
+
+  margin-bottom: 30px;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: #e9ecf2;
+  }
 `;
 
 const Tab = styled.div<{ active?: boolean }>`
   padding: 8px 4px;
+  position: relative;
+
+  border-bottom: ${(props) =>
+    props.active ? "2px solid #3fe7be" : "2px solid transparent"};
 
   span {
-    font-weight: ${(props) => (props.active ? "#3fe7be" : "#8a8f99")};
+    color: ${(props) => (props.active ? "#3fe7be" : "#8a8f99")};
     font-weight: ${(props) => (props.active ? "600" : "400")};
     font-size: 14px;
     line-height: 140%;
     font-size: 14px;
     line-height: 140%;
+  }
+`;
+
+const SettingsTitle = styled.span<{ margin: string }>`
+  font-family: "Inter";
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 140%;
+  margin-bottom: ${(props) => props.margin};
+  color: #8a8f99;
+`;
+
+const InputContainer = styled.div`
+  gap: 16px;
+  display: flex;
+
+  .title {
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 140%;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .input-wrapper {
+    width: 340px;
+    border: 1px solid #e9ecf2;
+    border-radius: 8px;
+    padding: 12px;
+    display: flex;
+  }
+
+  .input {
+    font-family: "Inter";
+    font-weight: 400;
+    font-size: 21px;
+    /* line-height: 30px; */
+    color: #c2c5cc;
+    text-align: right;
+
+    margin-left: auto;
+
+    &::placeholder {
+      font-family: "Inter";
+      font-weight: 400;
+      font-size: 21px;
+      line-height: 30px;
+      color: #c2c5cc;
+      text-align: right;
+    }
   }
 `;
 
